4ce1589fcd157417c178fad0a531ec2083ae973b
Tony Amirault <tony.amirault@tinyclues.com>
2020-06-12 19:11:43 +0200

    use https when http

--- server.js
+++ server.js
@@ -54,5 +54,14 @@ app.get('*', (req, res) => {
   );
 });
 
+if(process.env.PROD) {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
